b23b41f7f19c00ec0e28ada764e5f17ab8ab20e2
Danilo MM <rdmoronm@unjbg.edu.pe>
2024-12-03 08:38:35 -0500

    test 5

--- src/app/api/messages/[senderId]/[receiverId]/route.ts
+++ src/app/api/messages/[senderId]/[receiverId]/route.ts
@@ -1,9 +1,8 @@
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
 
-// Obtener los mensajes entre dos usuarios
 export async function GET(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { senderId: string; receiverId: string } }
 ) {
   const { senderId, receiverId } = params;
@@ -11,36 +10,20 @@ export async function GET(
   try {
     const messages = await prisma.message.findMany({
       where: {
-        OR: [
-          { senderId, receiverId },
-          { senderId: receiverId, receiverId: senderId },
-        ],
+        senderId,
+        receiverId,
       },
-      orderBy: { createdAt: "asc" },
+      orderBy: {
+        createdAt: "asc",
+      },
     });
 
     return NextResponse.json(messages);
   } catch (error) {
-    return NextResponse.error();
-  }
-}
-
-// Guardar un mensaje nuevo
-export async function POST(req: NextRequest) {
-  const { senderId, receiverId, content, timestamp } = await req.json();
-
-  try {
-    const newMessage = await prisma.message.create({
-      data: {
-        senderId,
-        receiverId,
-        content,
-        createdAt: new Date(timestamp),
-      },
-    });
-
-    return NextResponse.json(newMessage);
-  } catch (error) {
-    return NextResponse.error();
+    console.error(error);
+    return NextResponse.json(
+      { error: "Error al obtener los mensajes" },
+      { status: 500 }
+    );
   }
 }
